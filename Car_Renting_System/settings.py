1a3b71859f76e948181ea74bcec51f2b610eaa89
shaheermudassar <shaheer.mudassar13@gmail.com>
2024-03-26 05:01:42 +0500

    updated

--- Car_Renting_System/settings.py
+++ Car_Renting_System/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%f3z_ue)bvn^40%2s$aklvq!gktn!6f(h-w311^jx$%s37j%x2'
+# SECRET_KEY = 'django-insecure-%f3z_ue)bvn^40%2s$aklvq!gktn!6f(h-w311^jx$%s37j%x2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,23 +137,29 @@ WSGI_APPLICATION = 'Car_Renting_System.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': os.environ.get('SQL_NAME'),
-
-        'USER': os.environ.get('SQL_USER'),
-
-        'PASSWORD': os.environ.get('SQL_PASSWORD'),
-
-        'HOST': os.environ.get('HOST'),
-
-        'PORT': os.environ.get('PORT'),
-
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 }
+
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': os.environ.get('SQL_NAME'),
+
+#         'USER': os.environ.get('SQL_USER'),
+
+#         'PASSWORD': os.environ.get('SQL_PASSWORD'),
+
+#         'HOST': os.environ.get('HOST'),
+
+#         'PORT': os.environ.get('PORT'),
+
+#     }
+
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
