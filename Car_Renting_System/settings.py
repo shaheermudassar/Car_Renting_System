284b3ed1d072764beeebd9cd4b1c0ae47fe86b53
shaheermudassar <shaheer.mudassar13@gmail.com>
2024-04-03 22:58:07 +0500

    Update settings.py

--- Car_Renting_System/settings.py
+++ Car_Renting_System/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -207,7 +207,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/DaisyRoom/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'DaisyRoom')
