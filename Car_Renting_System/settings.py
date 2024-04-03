a3d604a410d11e8c62f7fad8c3c2ec2a32a195a3
shaheermudassar <shaheer.mudassar13@gmail.com>
2024-04-03 22:52:23 +0500

    Update settings.py

--- Car_Renting_System/settings.py
+++ Car_Renting_System/settings.py
@@ -137,14 +137,14 @@ WSGI_APPLICATION = 'Car_Renting_System.wsgi.application'
 # }
 
 # internal database
-# DATABASES = {
-#     'default': dj_database_url.parse("postgres://avnadmin:AVNS_aae2b_Q6skUVHWGBJo2@daisyroom-daisyroom.a.aivencloud.com:26643/defaultdb?sslmode=require") 
-# }
+DATABASES = {
+    'default': dj_database_url.parse("postgres://daisyroom:ycpwnaJ0auuMwYhYohchY1GNA8igV8wg@dpg-co3foagl5elc73dcm990-a/daisyroom_18o3") 
+}
 
 # external database
-DATABASES = {
-    'default': dj_database_url.parse("postgres://daisyroom:ycpwnaJ0auuMwYhYohchY1GNA8igV8wg@dpg-co3foagl5elc73dcm990-a.singapore-postgres.render.com/daisyroom_18o3") 
-}
+# DATABASES = {
+#     'default': dj_database_url.parse("postgres://daisyroom:ycpwnaJ0auuMwYhYohchY1GNA8igV8wg@dpg-co3foagl5elc73dcm990-a.singapore-postgres.render.com/daisyroom_18o3") 
+# }
 # DATABASES = {
 
 #     'default': {
