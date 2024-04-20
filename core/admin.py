a7790dc673d0e62cfa09782239bb9e36db4bb838
shaheermudassar <124002206+shaheermudassar@users.noreply.github.com>
2024-04-20 22:16:35 +0500

    updated

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from core.models import Customization
-
-class CustomizationAdmin(admin.ModelAdmin):
-    list_display = ["title"]
-    def has_add_permission(self, *args, **kwargs):
-        return not Customization.objects.exists()
-
-admin.site.register(Customization, CustomizationAdmin)
--- userauths/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from userauths.models import User
-
-# Register your models here.
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email']
-
-
-admin.site.register(User, UserAdmin)
